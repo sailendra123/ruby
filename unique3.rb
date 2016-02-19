3fd0efef781d03e165c9b3d0055ec4b1e028b0c8
sailendra123 <vs.sailendra@gmail.com>
2016-02-19 16:06:38 +0530

    day2 afternoon

--- unique3.rb
+++ unique3.rb
@@ -1,9 +1,11 @@
 animals = ['cat','dog','fish','fish']
 val = Array.new;
+val1 = Array.new;
 animals.each_with_index do |values,index|
    if(val!=values)
-  puts val = values
+   val = values
+   val1.push(values)
    end
 end
 
-
+puts val1
